8139d407404b4aca75b78e7a0ce36593badeb948
Octavio Farias <arturoesfeo888@gmail.com>
2024-12-07 00:18:09 -0300

    move koth verifications to ActiveKoth's scheduler() function

--- src/main/java/ar/com/octaviofarias/koth/listeners/KoTHListener.java
+++ src/main/java/ar/com/octaviofarias/koth/listeners/KoTHListener.java
@@ -3,15 +3,11 @@ package ar.com.octaviofarias.koth.listeners;
 import ar.com.octaviofarias.koth.DamiXKoTH;
 import ar.com.octaviofarias.koth.KoTHManager;
 import ar.com.octaviofarias.koth.commands.RewardsKoTHCommand;
-import ar.com.octaviofarias.koth.model.ActiveKoTH;
 import ar.com.octaviofarias.koth.model.KoTH;
-import ar.com.octaviofarias.koth.utils.KoTHUtils;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryCloseEvent;
-import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.ItemStack;
 
@@ -22,50 +18,6 @@ import java.util.List;
 import static ar.com.octaviofarias.koth.utils.KoTHUtils.sendMessage;
 
 public class KoTHListener implements Listener {
-
-
-    @EventHandler
-    public void onMove(PlayerMoveEvent e){
-        Player player = e.getPlayer();
-        for(ActiveKoTH koTH : KoTHManager.getActiveKoTHs()){
-            assert koTH.getKoTH().getFirstPosition() != null;
-            assert koTH.getKoTH().getSecondPosition() != null;
-            if(KoTHUtils.isLocationInCuboid(player.getLocation(), koTH.getKoTH().getFirstPosition(), koTH.getKoTH().getSecondPosition())){
-                if(koTH.getDominating() == player) return;
-                if(koTH.getDominating() == null){
-                    koTH.setDominatingTime(koTH.getKoTH().getCaptureTime());
-                    koTH.setDominating(player);
-                    for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
-                        for (String s : DamiXKoTH.getMessages().getMessageList("koth.now-dominating")) {
-                            sendMessage(onlinePlayer, s.replace("%player%", player.getName()).replace("%koth%", koTH.getKoTH().getName()));
-                        }
-                    }
-                    return;
-                }
-            }
-            if(koTH.getDominating() == player){
-                for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
-                    if(KoTHUtils.isLocationInCuboid(onlinePlayer.getLocation(), koTH.getKoTH().getFirstPosition(), koTH.getKoTH().getSecondPosition())) {
-                        koTH.setDominatingTime(koTH.getKoTH().getCaptureTime());
-                        koTH.setDominating(player);
-                        for (Player op : Bukkit.getOnlinePlayers()) {
-                            for (String s : DamiXKoTH.getMessages().getMessageList("koth.now-dominating")) {
-                                sendMessage(op, s.replace("%player%", onlinePlayer.getName()).replace("%koth%", koTH.getKoTH().getName()));
-                            }
-                        }
-                        return;
-                    }
-                }
-                koTH.setDominating(null);
-                koTH.setDominatingTime(koTH.getKoTH().getCaptureTime());
-                for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
-                    for (String s : DamiXKoTH.getMessages().getMessageList("koth.no-one-dominating")) {
-                        sendMessage(onlinePlayer, s.replace("%koth%", koTH.getKoTH().getName()));
-                    }
-                }
-            }
-        }
-    }
 
     @EventHandler
     public void onQuit(PlayerQuitEvent e){
--- src/main/java/ar/com/octaviofarias/koth/model/ActiveKoTH.java
+++ src/main/java/ar/com/octaviofarias/koth/model/ActiveKoTH.java
@@ -3,6 +3,7 @@ package ar.com.octaviofarias.koth.model;
 
 import ar.com.octaviofarias.koth.DamiXKoTH;
 import ar.com.octaviofarias.koth.KoTHManager;
+import ar.com.octaviofarias.koth.utils.KoTHUtils;
 import lombok.Getter;
 import lombok.Setter;
 import org.bukkit.Bukkit;
@@ -36,10 +37,51 @@ public class ActiveKoTH {
         reamingTime = activeTime;
         this.dominatingTime = koTH.getCaptureTime();
         task = Bukkit.getScheduler().runTaskTimer(DamiXKoTH.getInstance(), () -> {
+
             if(reamingTime < 0 || dominatingTime < 0) {
                 finish();
                 return;
             }
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                assert getKoTH().getFirstPosition() != null;
+                assert getKoTH().getSecondPosition() != null;
+                if(KoTHUtils.isLocationInCuboid(player.getLocation(), getKoTH().getFirstPosition(), getKoTH().getSecondPosition())){
+                    if(getDominating() == player) return;
+                    if(getDominating() == null){
+                        setDominatingTime(koTH.getCaptureTime());
+                        setDominating(player);
+                        for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+                            for (String s : DamiXKoTH.getMessages().getMessageList("koth.now-dominating")) {
+                                sendMessage(onlinePlayer, s.replace("%player%", player.getName()).replace("%koth%", getKoTH().getName()));
+                            }
+                        }
+                        return;
+                    }
+                }
+                if(getDominating() == player){
+                    for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+                        if(KoTHUtils.isLocationInCuboid(onlinePlayer.getLocation(), getKoTH().getFirstPosition(), getKoTH().getSecondPosition())) {
+                            setDominatingTime(koTH.getCaptureTime());
+                            setDominating(player);
+                            for (Player op : Bukkit.getOnlinePlayers()) {
+                                for (String s : DamiXKoTH.getMessages().getMessageList("koth.now-dominating")) {
+                                    sendMessage(op, s.replace("%player%", onlinePlayer.getName()).replace("%koth%", getKoTH().getName()));
+                                }
+                            }
+                            return;
+                        }
+                    }
+                    setDominating(null);
+                    setDominatingTime(getKoTH().getCaptureTime());
+                    for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+                        for (String s : DamiXKoTH.getMessages().getMessageList("koth.no-one-dominating")) {
+                            sendMessage(onlinePlayer, s.replace("%koth%", koTH.getName()));
+                        }
+                    }
+                }
+
+            }
+
             reamingTime--;
             if(dominating != null) dominatingTime--;
         }, 0L, 20L);
