c6c1aa6ecaa48a5507fa960b6896efc4cbe5ae9d
Octavio Farias <arturoesfeo888@gmail.com>
2024-12-09 23:42:14 -0300

    add command koth scheduler add

--- src/main/java/ar/com/octaviofarias/koth/commands/SchedulersKoTHCommand.java
+++ src/main/java/ar/com/octaviofarias/koth/commands/SchedulersKoTHCommand.java
@@ -7,8 +7,10 @@ import ar.com.octaviofarias.koth.model.KoTHScheduler;
 import org.apache.commons.lang3.StringUtils;
 import org.bukkit.command.CommandSender;
 
+import java.time.DayOfWeek;
 import java.util.List;
 
+import static ar.com.octaviofarias.koth.utils.KoTHUtils.sendCenteredMessage;
 import static ar.com.octaviofarias.koth.utils.KoTHUtils.sendMessage;
 
 public class SchedulersKoTHCommand implements KoTHSubCommand{
@@ -51,9 +53,69 @@ public class SchedulersKoTHCommand implements KoTHSubCommand{
                         .replace("%day%", StringUtils.capitalize(scheduler.getDay().toString().toLowerCase()))
                         .replace("%hour%", String.valueOf(scheduler.getHour()))
                         .replace("%minute%", String.valueOf(scheduler.getMinutes()))
-                        .replace("%second%", String.valueOf(scheduler.getSeconds())));
+                        .replace("%second%", String.valueOf(scheduler.getSeconds()))
+                        .replace("%duration%", String.valueOf(scheduler.getDuration())));
                 i++;
             }
+            return;
+        }else if(args[0].equalsIgnoreCase("add")){
+            if(args.length < 5){
+                sendMessage(sender, DamiXKoTH.getMessages().getMessage("commands.schedulers.add.usage"));
+                return;
+            }
+            KoTH koTH = KoTHManager.getKoTH(args[1]);
+            if(koTH == null){
+                sendMessage(sender, DamiXKoTH.getMessages().getMessage("commands.unknown-koth"));
+                return;
+            }
+
+            DayOfWeek day;
+            int hour, minute, second;
+
+            try {
+                day = DayOfWeek.valueOf(args[2]);
+            }catch (IllegalArgumentException e){
+                sendMessage(sender, DamiXKoTH.getMessages().getMessage("commands.schedulers.add.invalid-day-of-week"));
+                return;
+            }
+
+            String[] time = args[3].split(":");
+
+            if(time.length != 3){
+                sendMessage(sender, DamiXKoTH.getMessages().getMessage("commands.schedulers.add.invalid-time-format"));
+                return;
+            }
+
+            try {
+                hour = Integer.parseInt(time[0]);
+                minute = Integer.parseInt(time[1]);
+                second = Integer.parseInt(time[2]);
+            }catch (NumberFormatException e){
+                sendMessage(sender, DamiXKoTH.getMessages().getMessage("commands.schedulers.add.invalid-time-format"));
+                return;
+            }
+
+            int duration;
+
+            try{
+                duration = Integer.parseInt(args[4]);
+            }catch (NumberFormatException e){
+                sendMessage(sender, DamiXKoTH.getMessages().getMessage("commands.invalid-number"));
+                return;
+            }
+
+            koTH.getSchedulers().add(new KoTHScheduler(day, hour, minute, second, duration));
+            KoTHManager.update(koTH);
+
+            sendMessage(sender, DamiXKoTH.getMessages().getMessage("commands.schedulers.add.successfully")
+                    .replace("%name%", koTH.getName())
+                    .replace("%id%", String.valueOf(koTH.getSchedulers().size()-1))
+                    .replace("%day%", StringUtils.capitalize(day.name().toLowerCase()))
+                    .replace("%hour%", String.valueOf(hour))
+                    .replace("%minute%", String.valueOf(minute))
+                    .replace("%second%", String.valueOf(second))
+                    .replace("%duration%", String.valueOf(duration)));
+
             return;
         }
 
